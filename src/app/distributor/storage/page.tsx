3b7b13e8d5002335adb16b60f98e2d9935fc7205
Maheswar Mahapatra <maheswar2110@gmail.com>
2024-02-03 15:09:32 +0530

    distributor surplus, storage page done

--- src/app/distributor/storage/page.tsx
+++ src/app/distributor/storage/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import CropCard from "@/components/component/farmer/CropCard";
 import DistributorHeader from "@/components/component/distributor/DistributorHeader";
 import DistributorFilter from "@/components/component/distributor/DistributorFilter";
+import DistributorStorageCard from "@/components/component/distributor/DistributorStorageCard";
 import Link from "next/link";
 
 export default function Surplus() {
@@ -12,9 +13,9 @@ export default function Surplus() {
       <div className="w-full flex gap-4 flex-wrap">
       <Link
           className="flex items-center px-4 py-4 text-sm font-medium text-gray-700 hover:bg-gray-100 hover:rounded-lg dark:text-gray-200"
-          href="/distributor/surplus/details"
+          href="/distributor/storage/details"
         >
-          <span className="mx-4"><CropCard /></span>
+          <span className="mx-4"><DistributorStorageCard /></span>
       </Link>
         
         <CropCard />
